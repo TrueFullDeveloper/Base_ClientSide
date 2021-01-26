53787410197c5f02513ff2bd17267a62d42cf816
Sergey <sergey_0218@mail.ru>
2021-01-27 00:46:40 +0300

    Add general project structure and page routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import './App.css'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {Home} from './pages/Home'
+import {Login} from './pages/Login'
+import {Signup} from './pages/Signup'
+import {History} from './pages/History'
+import {Search} from './pages/Search'
+import {Profile} from './pages/Profile'
+import {PassRecovery} from './pages/PassRecovery'
 
 function App() {
   return (
-    <div />
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path={'/'} exact component={Home}/>
+          <Route path={'/login'} component={Login}/>
+          <Route path={'/signin'} component={Signup}/>
+          <Route path={'/history'} component={History}/>
+          <Route path={'/search'} component={Search}/>
+          <Route path={'/profile'} component={Profile}/>
+          <Route path={'/passwordrecovery'} component={PassRecovery}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
       
   )
 }
