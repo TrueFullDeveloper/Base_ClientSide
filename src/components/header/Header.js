a69ffcb443f4ee3baa558d4bb379e482df1609e4
SERGEY SELEZNEV <sergey_0218@mail.ru>
2021-05-23 17:39:56 +0700

    Add Styles for Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css";
+import logo from "../../static/images/header/logo.svg";
 
 export const Header = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/'>Главная</NavLink>
-        </li>
-        <li>
-          <NavLink to='/topqueries'>Топ запросов</NavLink>
-        </li>
-        <li>
-          <NavLink to='/history'>История</NavLink>
-        </li>
-        <li>
-          <NavLink to='/profile'>Профиль</NavLink>
-        </li>
-      </ul>
-    </nav>
+    <div className={styles.header}>
+      <div className={styles.first_section}>
+        <div>
+          <NavLink to="/">
+            <img src={logo} alt="nothing" />
+          </NavLink>
+        </div>
+        <div className={`${styles.group_links} ${styles.first}`}>
+          <NavLink to="/topqueries">Топ запросов</NavLink>
+        </div>
+        <div className={`${styles.group_links} ${styles.second}`}>
+          <NavLink to="/history">История</NavLink>
+        </div>
+        <div className={`${styles.group_links} ${styles.third}`}>
+          <a href="#">Настройки</a>
+        </div>
+      </div>
+      <div className={styles.second_section}>
+        <NavLink to="/profile">Профиль</NavLink>
+      </div>
+    </div>
   );
 };
